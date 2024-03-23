3d6e0f86bfd8e92347758daa7b7438c35078ffa0
Faizan Raza <fazykn@users.noreply.github.com>
2024-03-23 15:42:40 +0000

    added Vercel Analytics 📈

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
